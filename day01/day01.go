f6766929385c61a25fdd1079ea5261e637bd24eb
Jakub Głazik <rzabcio@gmail.com>
2025-12-19 16:27:00 +0100

    wip: day01_2

--- day01/day01.go
+++ day01/day01.go
@@ -3,6 +3,7 @@ package day01
 
 import (
 	"fmt"
+	"math"
 	"strconv"
 	"strings"
 
@@ -32,13 +33,61 @@ func Day01_1(filename string) (result int) {
 	return result
 }
 
-func Day01_2(filename string) (result int) {
+func Day01_3(filename string) (result int) {
+	current := 50
 	for line := range utils.InputRows(filename) {
-		// split line to letter and number, example: "R14" -> "R" and "14"
 		parts := strings.SplitAfterN(line, "", 2)
 		num, _ := strconv.Atoi(parts[1])
-		fmt.Println(parts[0], num)
+		fmt.Printf("%s%s: %d", parts[0], parts[1], current)
+		if parts[0] == "R" {
+			current += num
+			fmt.Printf(" + %d -> %d", num, current)
+		} else if parts[0] == "L" {
+			current -= num
+			fmt.Printf(" - %d -> %d", num, current)
+		}
+		for current >= 100 {
+			current -= 100
+			result++
+			fmt.Printf(" - 100 -> %d [%d]", current, result)
+		}
+		for current < 0 {
+			current += 100
+			result++
+			fmt.Printf(" + 100 -> %d [%d]", current, result)
+		}
+		fmt.Println()
 	}
+	return result
+}
 
+func Day01_2(filename string) (result int) {
+	current := 50
+	for line := range utils.InputRows(filename) {
+		parts := strings.SplitAfterN(line, "", 2)
+		num, _ := strconv.Atoi(parts[1])
+		fmt.Printf("%s%s: %d", parts[0], parts[1], current)
+		if parts[0] == "R" {
+			if current == 0 {
+				result++
+			}
+			current += num
+			fmt.Printf(" + %d -> %d", num, current)
+		} else if parts[0] == "L" {
+			current -= num
+			fmt.Printf(" - %d -> %d", num, current)
+		}
+		currentMod := current % 100
+		currentDiv := current / 100
+		fmt.Printf(" => mod: %d, div: %d", currentMod, currentDiv)
+		if currentMod < 0 {
+			currentMod += 100
+			currentDiv--
+			fmt.Printf(" => mod: %d, div: %d", currentMod, currentDiv)
+		}
+		current = currentMod
+		result += int(math.Abs(float64(currentDiv)))
+		fmt.Println()
+	}
 	return result
 }
--- main_test.go
+++ main_test.go
@@ -7,11 +7,11 @@ import (
 )
 
 func TestDay01(t *testing.T) {
-	got, want := day01.Day01_1("input-files/day01-test1"), 0
+	got, want := day01.Day01_1("input-files/day01-test1"), 3
 	if got != want {
 		t.Errorf("Day01_1(test1) = %d; want %d", got, want)
 	}
-	got, want = day01.Day01_2("input-files/day01-test1"), 0
+	got, want = day01.Day01_2("input-files/day01-test1"), 6
 	if got != want {
 		t.Errorf("Day01_2(test1) = %d; want %d", got, want)
 	}
